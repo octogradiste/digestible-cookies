03af8e3e69bd76cf7fcd194a81ba5efe2cba7f82
octogradiste <colin.berger@outlook.com>
2025-03-26 11:53:55 +0100

    Formatting and update README.md

--- src/components/buttons/accept-reject-action-button.tsx
+++ src/components/buttons/accept-reject-action-button.tsx
@@ -8,15 +8,15 @@ interface AcceptRejectActionButtonProps {
   onAction?: () => void;
 }
 
-export default function AcceptRejectActionButton({actionTitle, longActionTitle, onAccept, onReject, onAction}: AcceptRejectActionButtonProps) {
+export default function AcceptRejectActionButton({ actionTitle, longActionTitle, onAccept, onReject, onAction }: AcceptRejectActionButtonProps) {
   return (
-  <div>
-    <OutlineButton title={longActionTitle} onClick={onAction} className="sm:hidden"/>
-    <div className="mb-2 flex justify-between gap-2 grid-cols-3">
-      <OutlineButton title="Reject All" onClick={onReject}/>
-      <OutlineButton title={actionTitle} onClick={onAction} className="hidden sm:block"/>
-      <OutlineButton title="Accept All" onClick={onAccept} />
+    <div>
+      <OutlineButton title={longActionTitle} onClick={onAction} className="sm:hidden" />
+      <div className="mb-2 flex justify-between gap-2 grid-cols-3">
+        <OutlineButton title="Reject All" onClick={onReject} />
+        <OutlineButton title={actionTitle} onClick={onAction} className="hidden sm:block" />
+        <OutlineButton title="Accept All" onClick={onAccept} />
+      </div>
     </div>
-  </div>
   );
 }
